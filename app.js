674e251480d34ae4a197707ed9612326192e18fe
kfrancois <koen.francois@hotmail.com>
2017-12-07 13:42:42 +0100

    Heroku deployment

--- app.js
+++ app.js
@@ -36,16 +36,15 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/API', index);
 app.use('/API/users', users);
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.all('*', (req, res) => {
+  const indexFile = `${path.join(__dirname, 'dist')}/index.html`;
+  res.status(200).sendFile(indexFile);
 });
 
 // error handler
